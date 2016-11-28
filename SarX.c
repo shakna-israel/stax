82fea037f32209287772a8b347d81836483b3989
James Milne <s4b3r6@gmail.com>
2016-11-29 02:13:12 +1100

    Complete rewrite
    Massive rewrite, pushing operation logic into a switch statement.
    
    Uses 24% less memory.

--- SarX.c
+++ SarX.c
@@ -1,120 +1,155 @@
-// Macro to remove boilerplate, as all math functions are pretty much identical.
-#define math(__name, __op) void __name() { int a = stack[stack_ptr]; stack_next(); int b = stack[stack_ptr]; stack[stack_ptr] = a __op b; stack_previous(); }
-#define STACK_SIZE 400
+#define STACK_SIZE 200
+#define FORWARD 0
+#define REVERSE -1
 
+char serialdata[120];
+int program[120];
 int program_ptr = 0;
-int stack[STACK_SIZE]; // All the memory we have!
+int stack[STACK_SIZE];
 int stack_ptr = STACK_SIZE + 1;
-int stack_dir = 0;
+int stack_dir = FORWARD;
 
-//
-void stack_next() {
-  if (stack_dir == 0) {
+enum OPS {
+  PUSH,
+  DROP,
+  DUP,
+  SWAP,
+  REV,
+  OUT,
+  NEW,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  GOTO
+};
+
+void clear_data() {
+  memset(serialdata, 0, sizeof(serialdata));
+}
+
+int data_exists() {
+  if (serialdata[0] != 0) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+// Move along the stack
+// stack_move
+void smv(int dir) {
+  if (dir == FORWARD) {
     stack_ptr--;
   } else {
     stack_ptr++;
   }
-  return;
-}
-
-//
-void stack_previous() {
-  if (stack_dir == 0) {
-    stack_ptr--;
-  } else {
-    stack_ptr++;
+  if (stack_ptr > STACK_SIZE + 1) {
+    stack_ptr = stack_ptr - (STACK_SIZE + 2);
+    return;
   }
-  return;
-}
-
-//goto
-void _goto(int i) {
-  program_ptr = i;
-  return;
-}
-
-//:label
-// Need to build some sort of array/s here
-
-//div
-math(div, / )
-
-//mul
-math(mul, *)
-
-//sub
-math(sub, -)
-
-//add
-math(add, +)
-
-//new
-
-//out
-void out() {
-  if (stack_dir == 0) {
-    for (int i = 0; i < STACK_SIZE; i++) {
-      if (stack[i] != -1) {
-        Serial.print((char)stack[i]);
-      }
-    }
-  } else {
-    for (int i = STACK_SIZE; i > 0; i--) {
-      if (stack[i] != -1) {
-        Serial.print((char)stack[i]);
-      }
-    }
+  if (stack_ptr < 0) {
+    stack_ptr = stack_ptr + (STACK_SIZE + 2);
+    return;
   }
 }
 
-//rev
-void rev() {
-  stack_dir = 1;
-}
-
-//swap
-void swap() {
-  int a = stack[stack_ptr];
-  stack_previous();
-  int b = stack[stack_ptr];
-}
-
-//dup
-void dup() {
-  int a = stack[stack_ptr];
-  stack_next();
-  stack[stack_ptr] = a;
-  return;
-}
-
-//drop
-void drop() {
-  stack[stack_ptr] = -1;
-  stack_next();
-  return;
-}
-
-//push
-void push(int a) {
-  stack_next();
-  stack[stack_ptr] = a;
-  return;
+// Execute some code
+// stack_execute
+void sex(int n) {
+  switch((OPS)n) {
+    // TODO: Push onto the top of the stack
+    // Needs to read an argument...
+    case(PUSH): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    // Drop the top of the stack
+    case(DROP): {
+      stack[stack_ptr] = -1;
+      smv(REVERSE);
+      break;
+    }
+    // Copy the top of the stack and push onto the stack
+    case(DUP): {
+      int t = stack[stack_ptr];
+      smv(FORWARD);
+      stack[stack_ptr] = t;
+      break;
+    }
+    // TODO: Swaps the top two values of the stack
+    case(SWAP): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    // TODO: Fakes flipping the stack
+    case(REV): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    // TODO: Prints the stack to stdout
+    case(OUT): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    // TODO: Pushes a character via Serial input
+    case(NEW): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    // TODO: drops the first two items from the stack, and pushes their sum to the top of the stack.
+    case(ADD): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    // TODO: drops the first two items from the stack, and pushes their sum to the top of the stack.
+    case(SUB): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    // TODO: drops the first two items from the stack, and pushes their product to the top of the stack.
+    case(MUL): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    // TODO: drops the first two items from the stack, and pushes their quotient to the top of the stack.
+    case(DIV): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    // TODO: takes a label parameter, and jumps to that label in the execution loop.
+    // Needs to read an argument...
+    case(GOTO): {
+      Serial.println("Not Yet Implemented.");
+      break;
+    }
+    default: {
+      Serial.print("Failed: ");
+      Serial.println(n);
+    }
+  }
 }
 
 void setup() {
-  //Initialise the stack
-  for (int i = 0; i < STACK_SIZE; i ++) {
+  for(int i = 0; i < STACK_SIZE; i++) {
     stack[i] = -1;
   }
-  for (int i = 20; i > 0; i--) {
-    push(65);
-  }
-  // Enable serial connections
   Serial.begin(9600);
-  Serial.println("Welcome");
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
 }
 
 void loop() {
-  out();
-  Serial.println("\nLoop fin.");
+  Serial.setTimeout(50);
+  Serial.readBytesUntil(10, serialdata, 80); // 10 == \lf
+  if (data_exists() == 0) {
+    Serial.println(serialdata);
+    for(int i = 0; i < strlen(serialdata); i++) {
+      sex(serialdata[i] - '0');
+    }
+    clear_data();
+  } else {
+    
+  }
 }
