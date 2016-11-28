2e81b486d33f229eefcd29d5e3fd579c6c0655b8
James Milne <s4b3r6@gmail.com>
2016-11-29 02:51:23 +1100

    Reduce footprint by F macro for strings.
    Reduces overall footprint in SRAM by 4% by using the F macro. Reduces Sketch size by 1%.

--- SarX.c
+++ SarX.c
@@ -57,81 +57,81 @@ void smv(int dir) {
 // Execute some code
 // stack_execute
 void sex(int n) {
-  switch((OPS)n) {
+  switch ((OPS)n) {
     // TODO: Push onto the top of the stack
     // Needs to read an argument...
-    case(PUSH): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (PUSH): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     // Drop the top of the stack
-    case(DROP): {
-      stack[stack_ptr] = -1;
-      smv(REVERSE);
-      break;
-    }
+    case (DROP): {
+        stack[stack_ptr] = -1;
+        smv(REVERSE);
+        break;
+      }
     // Copy the top of the stack and push onto the stack
-    case(DUP): {
-      int t = stack[stack_ptr];
-      smv(FORWARD);
-      stack[stack_ptr] = t;
-      break;
-    }
+    case (DUP): {
+        int t = stack[stack_ptr];
+        smv(FORWARD);
+        stack[stack_ptr] = t;
+        break;
+      }
     // TODO: Swaps the top two values of the stack
-    case(SWAP): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (SWAP): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     // TODO: Fakes flipping the stack
-    case(REV): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (REV): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     // TODO: Prints the stack to stdout
-    case(OUT): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (OUT): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     // TODO: Pushes a character via Serial input
-    case(NEW): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (NEW): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     // TODO: drops the first two items from the stack, and pushes their sum to the top of the stack.
-    case(ADD): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (ADD): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     // TODO: drops the first two items from the stack, and pushes their sum to the top of the stack.
-    case(SUB): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (SUB): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     // TODO: drops the first two items from the stack, and pushes their product to the top of the stack.
-    case(MUL): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (MUL): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     // TODO: drops the first two items from the stack, and pushes their quotient to the top of the stack.
-    case(DIV): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (DIV): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     // TODO: takes a label parameter, and jumps to that label in the execution loop.
     // Needs to read an argument...
-    case(GOTO): {
-      Serial.println("Not Yet Implemented.");
-      break;
-    }
+    case (GOTO): {
+        Serial.println(F("Not Yet Implemented."));
+        break;
+      }
     default: {
-      Serial.print("Failed: ");
-      Serial.println(n);
-    }
+        Serial.print(F("Failed: "));
+        Serial.println(n);
+      }
   }
 }
 
 void setup() {
-  for(int i = 0; i < STACK_SIZE; i++) {
+  for (int i = 0; i < STACK_SIZE; i++) {
     stack[i] = -1;
   }
   Serial.begin(9600);
@@ -141,15 +141,15 @@ void setup() {
 }
 
 void loop() {
-  Serial.setTimeout(50);
+  Serial.setTimeout(50); // Don't block the loop waiting for input!
   Serial.readBytesUntil(10, serialdata, 80); // 10 == \lf
   if (data_exists() == 0) {
     Serial.println(serialdata);
-    for(int i = 0; i < strlen(serialdata); i++) {
+    for (int i = 0; i < strlen(serialdata); i++) {
       sex(serialdata[i] - '0');
     }
     clear_data();
   } else {
-    
+
   }
 }
